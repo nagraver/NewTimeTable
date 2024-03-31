9d110aa1750d55bda6cd4365c196d77310db51bc
nagraver <ttolla25@gmail.com>
2024-04-01 01:28:30 +0300

    Fifteenth commit

--- logic/get_dict.py
+++ logic/get_dict.py
@@ -1,16 +1,16 @@
-import requests
-import json
-from bs4 import BeautifulSoup
+import httpx
 from datetime import datetime
 from urllib.parse import quote_plus
+import asyncio
 
 
 async def get_groups(inst):
     url = 'https://sevsu.samrzhevsky.ru/api/groups?v=3.1&section=0&institute=' + inst
-    response = requests.get(url).text
-    bs = BeautifulSoup(response, "lxml").text
+    async with httpx.AsyncClient() as client:
+        task = client.get(url)
+        response = await asyncio.create_task(task)
 
-    return json.loads(bs)['groups']
+    return response.json().get('groups')
 
 
 async def get_schedule(inst, group, the_day):
@@ -19,11 +19,12 @@ async def get_schedule(inst, group, the_day):
 
     url = 'https://sevsu.samrzhevsky.ru/api/schedule?v=3.1&section=0&institute=' + inst + '&group=' + str(
         quote_plus(group)) + '&week=' + str(week_number)
-    response = requests.get(url).text
-    bs = BeautifulSoup(response, "lxml").text
+    async with httpx.AsyncClient() as client:
+        task = client.get(url)
+        response = await asyncio.create_task(task)
 
     try:
-        return json.loads(bs)['schedule']
+        return response.json().get('schedule')
+
     except KeyError:
         return None
-
